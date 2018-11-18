e36686f47eaad81a4babe488ac318aef552aa1e5
Ben Ehlert <benjamin.ehlert@gmail.com>
2018-11-17 21:11:42 -0700

    changed item back to functional component

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,33 +1,24 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './main.css';
 
-class Item extends Component {
-   constructor(props){
-      super(props)
-      this.state = {
-         
-      }  
-   }
-
-render(props){
+const Item = (props) => {
    const { userId, id, title, completed, onDelete, toggleDone } = props;
    const doneClass = completed ? 'done' : '';
    let deleteBtnClass = 'deleteBtn toggleBtn'
-      return(
-         <div className='container'>
-            <button
-               className={deleteBtnClass}
-               onClick={onDelete}
-            >x</button>
-            <li 
-               onClick={toggleDone} 
-               className={doneClass}
-               onMouseEnter={(e)=> console.log(e)}
-               onMouseLeave={(e)=> console.log(e)}
-            >{title}</li>
-         </div>
-      )
-   }
+   return(
+      <div className='container'>
+          <button
+            className={deleteBtnClass}
+            onClick={onDelete}
+         >x</button>
+         <li 
+            onClick={toggleDone} 
+            className={doneClass}
+            onMouseEnter={(e)=> console.log(e)}
+            onMouseLeave={(e)=> console.log(e)}
+         >{title}</li>
+      </div>
+   )
 }
 
 export default Item;
